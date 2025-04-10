be148e6c2eb56e6a12e508997361a3cd3b62b383
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-04-10 14:02:19 +0200

    init commit nextjs

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <>
+      <h1>hej</h1>
+      <Button>Click me</Button>
+    </>
+  );
+}
