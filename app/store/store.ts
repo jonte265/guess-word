5fb5578a0eb6b6b757aefe86018206c245039d00
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-04-10 15:35:23 +0200

    gamestart random word

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,23 +1,26 @@
-import alphabet from '@/app/data/alphabet.json';
+import words from '@/app/data/words.json';
 import { create } from 'zustand';
 
 type wordStoreType = {
   count: number;
   chosenWord: string;
+  gameStart: boolean;
   increment: () => void;
-  randomWord: () => void;
+  startGame: () => void;
 };
 
 const useWordStore = create<wordStoreType>((set) => ({
   count: 0,
   chosenWord: 'default',
+  gameStart: false,
 
   increment: () => set((state) => ({ count: state.count + 1 })),
 
-  randomWord: () =>
+  startGame: () =>
     set(() => {
       return {
-        chosenWord: 'Banan',
+        gameStart: true,
+        chosenWord: words[Math.floor(Math.random() * words.length)],
       };
     }),
 }));
--- components/ui/custom/GameArea.tsx
+++ components/ui/custom/GameArea.tsx
@@ -5,6 +5,7 @@ import words from '@/app/data/words.json';
 import alphabet from '@/app/data/alphabet.json';
 import alphabetKeyboard from '@/app/data/alphabetKeyboard.json';
 import useWordStore from '@/app/store/store';
+import { useEffect } from 'react';
 
 type GameAreaType = {
   alp: string;
@@ -14,20 +15,25 @@ function GameArea() {
   const wordStore = useWordStore();
 
   return (
-    <section className='flex flex-col justify-center items-center max-w-2xl m-auto'>
-      {/* KeyBoard */}
-      <div className='flex flex-wrap justify-evenly items-center w-120'>
-        {alphabetKeyboard.map((alp: string, index: number) => {
-          return (
-            <Button key={index} variant='outline'>
-              {alp}
-            </Button>
-          );
-        })}
-      </div>
-
-      <Button onClick={wordStore.increment}>Click me</Button>
-      <p>{wordStore.count}</p>
+    <section className='flex flex-col gap-4 justify-center items-center max-w-2xl m-auto'>
+      {wordStore.gameStart ? (
+        <>
+          {/* Question area */}
+          <h1 className='text-2xl'>{wordStore.chosenWord}</h1>
+          {/* Keyboard */}
+          <div className='flex flex-wrap justify-evenly items-center w-120'>
+            {alphabetKeyboard.map((alp: string, index: number) => {
+              return (
+                <Button key={index} variant='outline'>
+                  {alp}
+                </Button>
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <Button onClick={wordStore.startGame}>Start</Button>
+      )}
     </section>
   );
 }
