6acdc5a00e8f2ca5d86371b2a8c55f321a27bb76
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-04-14 01:49:54 +0200

    fix errors

--- components/ui/custom/HeaderNav.tsx
+++ components/ui/custom/HeaderNav.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button } from '@/components/ui/button';
+
 import useWordStore from '@/app/store/store';
 
 function HeaderNav() {
@@ -8,19 +8,7 @@ function HeaderNav() {
   return (
     <header className='flex flex-row flex-wrap justify-between items-center p-4 max-w-2xl mx-auto'>
       <h2 className='font-semibold'>Ordle</h2>
-      {/* <nav className=''>
-        <ul className='flex gap-4'>
-          <li>
-            <Button variant='outline'>Hej</Button>
-          </li>
-          <li>
-            <Button variant='outline'>Hej</Button>
-          </li>
-          <li>
-            <Button variant='outline'>Hej</Button>
-          </li>
-        </ul>
-      </nav> */}
+
       <p>
         Win streak: <strong>{wordStore.winStreak}</strong>
       </p>
