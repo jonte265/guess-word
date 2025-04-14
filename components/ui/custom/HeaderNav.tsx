7d98127459e89d39fa13d20a5f6795185586ced0
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-04-14 21:03:04 +0200

    add shadcn darkmode

--- components/ui/custom/HeaderNav.tsx
+++ components/ui/custom/HeaderNav.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import useWordStore from '@/app/store/store';
+import { ModeToggle } from '../ModeToggle';
 
 function HeaderNav() {
   const wordStore = useWordStore();
@@ -8,10 +9,10 @@ function HeaderNav() {
   return (
     <header className='flex flex-row flex-wrap justify-between items-center p-4 max-w-2xl mx-auto'>
       <h2 className='font-semibold'>Ordle</h2>
-
       <p>
         Win streak: <strong>{wordStore.winStreak} 🏆</strong>
       </p>
+      <ModeToggle />
     </header>
   );
 }
