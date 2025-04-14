76873c61c716761e1881de46cef104acd132d4e3
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-04-14 21:24:38 +0200

    fix letterbox border color dark

--- components/ui/custom/LetterBox.tsx
+++ components/ui/custom/LetterBox.tsx
@@ -18,7 +18,7 @@ function LetterBox({ letter, status }: LetterBoxType) {
 
   return (
     <div
-      className={`flex justify-center items-center border-2 p-4 w-12 h-16 text-center ${backgroundColor}`}
+      className={`flex justify-center items-center border-2 dark:border-[#e5e5e5] p-4 w-12 h-16 text-center ${backgroundColor}`}
     >
       {/* If no letter add empty space  */}
       <p className='font-semibold text-[#0a0a0a]'>{letter.toUpperCase()}</p>
