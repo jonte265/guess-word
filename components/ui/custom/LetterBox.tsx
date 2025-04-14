d699e66aef75241872d680885e30a876467d7743
jonte265 <200072657+jonte265@users.noreply.github.com>
2025-04-14 21:54:06 +0200

    fix darkmode letterbox colors contrast

--- components/ui/custom/LetterBox.tsx
+++ components/ui/custom/LetterBox.tsx
@@ -5,23 +5,28 @@ type LetterBoxType = {
 
 function LetterBox({ letter, status }: LetterBoxType) {
   let backgroundColor;
+  let textColor;
 
   if (status === 0) {
-    backgroundColor = 'bg-white'; // Neutral
+    backgroundColor = 'bg-white dark:bg-gray-900'; // Neutral
+    textColor = 'text-black dark:text-white';
   } else if (status === 1) {
-    backgroundColor = 'bg-gray-100'; // Incorrect
+    backgroundColor = 'bg-gray-200 dark:bg-gray-700'; // Incorrect
+    textColor = 'text-black dark:text-white';
   } else if (status === 2) {
-    backgroundColor = 'bg-yellow-500'; // Partial
+    backgroundColor = 'bg-yellow-500 dark:bg-yellow-300'; // Partial
+    textColor = 'text-black dark:text-black';
   } else if (status === 3) {
-    backgroundColor = 'bg-green-500'; // Match
+    backgroundColor = 'bg-green-500 dark:bg-green-400'; // Match
+    textColor = 'text-black dark:text-black';
   }
 
   return (
     <div
-      className={`flex justify-center items-center border-2 dark:border-[#e5e5e5] p-4 w-12 h-16 text-center ${backgroundColor}`}
+      className={`flex justify-center items-center border-2 dark:border-gray-500 p-4 w-12 h-16 text-center ${backgroundColor}`}
     >
-      {/* If no letter add empty space  */}
-      <p className='font-semibold text-[#0a0a0a]'>{letter.toUpperCase()}</p>
+      {/* If no letter add empty space */}
+      <p className={`font-semibold ${textColor}`}>{letter.toUpperCase()}</p>
     </div>
   );
 }
