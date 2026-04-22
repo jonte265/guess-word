4cb77e8f10062336acb2cdf3de182a9fb6d232c3
jonte265 <200072657+jonte265@users.noreply.github.com>
2026-04-22 17:33:38 +0200

    header logo bold

--- components/ui/custom/HeaderNav.tsx
+++ components/ui/custom/HeaderNav.tsx
@@ -1,14 +1,17 @@
-'use client';
+"use client";
 
-import useWordStore from '@/app/store/store';
-import { ModeToggle } from '../ModeToggle';
+import useWordStore from "@/app/store/store";
+import { ModeToggle } from "../ModeToggle";
+import Link from "next/link";
 
-function HeaderNav() {
+export default function HeaderNav() {
   const wordStore = useWordStore();
 
   return (
-    <header className='flex flex-row flex-wrap justify-between items-center p-4 max-w-2xl mx-auto'>
-      <h2 className='font-semibold'>Ordle</h2>
+    <header className="flex flex-row flex-wrap justify-between items-center p-4 max-w-2xl mx-auto">
+      <Link href="/">
+        <h2 className="font-bold">Ordle</h2>
+      </Link>
       <p>
         Win streak: <strong>{wordStore.winStreak} 🏆</strong>
       </p>
@@ -16,5 +19,3 @@ function HeaderNav() {
     </header>
   );
 }
-
-export default HeaderNav;
